4ee524afe1738cccca468acd24db1168b077ed4e
Christian Velasquez <amnesiacsloth@gmail.com>
2022-10-30 23:59:14 -0700

    commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Lab 5 - Starter
+
+Christian Velasquez
+
+amnesiacsloth.github.io/Lab5_starter/expose.html
+amnesiacsloth.github.io/Lab5_starter/explore.html
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,43 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-function init() {
-  console.log("hi");
+
+// HELPER FUNCTION
+function fillVoices() {
+
+  const voiceOptions = speechSynthesis.getVoices();
+
+  for (let i = 0; i < voiceOptions.length; i++) {
+
+    const option = document.createElement('option');
+    option.textContent = `${voiceOptions[i].name} (${voiceOptions[i].lang})`;
+
+    option.setAttribute('data-lang', voiceOptions[i].lang);
+    option.setAttribute('data-name', voiceOptions[i].name);
+
+    document.getElementById("voice-select").appendChild(option);
+  }
 }
+
+const smiley = document.querySelector("img");
+const playButton = document.querySelector("button"); 
+var getText = document.getElementById("test-to-speak");
+
+function init() {
+  fillVoices();
+
+  // TODO 
+  if (true) {
+    smiley.src = 'assets/images/smiling-open.png' 
+  }
+
+
+  playButton.addEventListener('click' , (event) => {
+    var toSpeak = new SpeechSynthesisUtterance(getText.value);
+
+    speechSynthesis.speak(utter);
+  });
+}
+
+
+
